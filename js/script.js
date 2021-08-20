995e67f102ee18ac51aa7d539d979da4b3e580f7
misaruiz <misaruiz@gmail.com>
2021-08-20 09:31:14 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,11 +17,14 @@ This function will create and insert/append the elements needed to display a "pa
 */
 
 function showPage(list, page){
+   // this makes groups of 9 students per page
    var startIndex = (page * 9) - 9;
    var endIndex = page * 9;
    const studentList = document.querySelector('ul.student-list');
    studentList.innerHTML = '';
+   // this loops through each student in the data script and creates a list item with all the student details
    for (i=0;i<list.length;i++){
+      // this checks which students are within the startIndex and endIndex range and list them only, list will change with pagination
       if(i>=startIndex && i<endIndex){
          const studentItem = `
          <li class="student-item cf">
@@ -47,24 +50,31 @@ This function will create and insert/append the elements needed for the paginati
 */
 
 function addPagination(list) {
+   // this determains the amount of pages, thus the amount of pagination buttons
    let numOfPages = Math.ceil(list.length/9);
    const linkList = document.querySelector('ul.link-list');
    linkList.innerHTML = '';
+   // this loops through the amount of pages determined above and creates a button with the page number
    for (i=1;i<=numOfPages;i++){
       let button = `<li><button type="button">${i}</button></li>`;
       linkList.insertAdjacentHTML('beforeend', button);
       var activeButton =  document.querySelector('button');
       activeButton.className = 'active';
+      // this eventListerner waits for a click and executes a function only if it is a button
       linkList.addEventListener("click", (e) => {
          if (e.target.tagName === 'BUTTON') {
+            // this looks for the active button and removes the active class
             var deactiveButton = document.querySelector('.active');
             deactiveButton.className = '';
+            // this adds the active class to button that was clicked (there has to be a way to know if the button clicked is already active so it doesn't remove active class and then add it again)
             e.target.className = 'active';
+            // when a button is clicked this executes the showPage function again with the new target page number
             showPage(data, e.target.textContent);
          };
       });
    };
  };
 
+ //this calls both functions on page load
 addPagination(data);
 showPage(data, 1);
